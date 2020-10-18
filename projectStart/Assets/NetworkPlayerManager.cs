50539c92d9eeb247505b57f46a7c325874b263ac
seamanc <49496503+seamanc@users.noreply.github.com>
2020-10-18 05:44:42 -0700

    Added avatar

--- projectStart/Assets/NetworkPlayerManager.cs
+++ projectStart/Assets/NetworkPlayerManager.cs
@@ -8,20 +8,28 @@ public class NetworkPlayerManager : MonoBehaviourPun
     // Start is called before the first frame update
     public GameObject rhand;
     public GameObject lhand;
+    public GameObject head;
+    public GameObject body;
+
 
     public SkinnedMeshRenderer rmesh;
     public SkinnedMeshRenderer lmesh;
+    public MeshRenderer hmesh;
+    public MeshRenderer bmesh;
     GameObject rpos;
     GameObject lpos;
+    GameObject hpos;
 
     void Start()
     {
         rpos = GameObject.FindGameObjectWithTag("righthand");
         lpos = GameObject.FindGameObjectWithTag("lefthand");
+        hpos = GameObject.FindGameObjectWithTag("head");
         if (photonView.IsMine)
         {
             rmesh.enabled = false;
             lmesh.enabled = false;
+            hmesh.enabled = false;
         }
     }
 
@@ -38,5 +46,10 @@ public class NetworkPlayerManager : MonoBehaviourPun
 
         rhand.transform.rotation = rpos.transform.rotation;
         lhand.transform.rotation = lpos.transform.rotation;
+
+        head.transform.position = hpos.transform.position;
+        head.transform.rotation = hpos.transform.rotation * Quaternion.Euler(0f, 180f, 0f);
+
+        body.transform.position = head.transform.position - new Vector3(0, .5f, 0);
     }
 }
