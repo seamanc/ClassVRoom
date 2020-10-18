066723c5af06791193a720b306aa63b35f59326a
seamanc <49496503+seamanc@users.noreply.github.com>
2020-10-17 21:13:07 -0700

    networking

--- projectStart/Assets/Scripts/GameManager.cs
+++ projectStart/Assets/Scripts/GameManager.cs
@@ -13,6 +13,9 @@ namespace Com.MyCompany.MyGame
 {
     public class GameManager : MonoBehaviourPunCallbacks
     {
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+
         #region Private Methods
 
 
@@ -83,5 +86,19 @@ namespace Com.MyCompany.MyGame
 
 
         #endregion
+
+        public void Start()
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+            }
+            else
+            {
+                Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
+            }
+        }
     }
 }
